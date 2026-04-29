10dba16fe169b7cd093c8fcfa06d0e7a1a42bf7c
Rashid <rashid18008@outlook.com>
2026-04-29 13:11:52 -0300

    update(usage): add --help and --version

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "cpu.h"
 #include "ram.h"
@@ -7,11 +8,13 @@
 #include "loader.h"
 #include "run.h"
 
+#define TOY_CPU_VERSION "Toy CPU 0.1.0 (Developed by: Rashid)."
+
 void usage() {
-  printf("Usage: toycpu [options] File...\n");
+  printf("Usage: toycpu File [options]...\n");
   printf("Options:\n");
-  printf("  --help\n");
-  printf("  --version\n");
+  printf("  --help             Display this information.\n");
+  printf("  --version          Display compiler version information.\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -21,6 +24,24 @@ int main(int argc, char* argv[]) {
   }
 
   char* file = argv[1];
+
+  if (argc > 2) {
+    char* option = argv[2];
+
+    if (strcmp(option, "--help") == 0) {
+      usage();
+      exit(0);
+    }
+    else if (strcmp(option, "--version") == 0) {
+      puts(TOY_CPU_VERSION);
+      exit(0);
+    }
+    else {
+      printf("unknown option: `%s`.\n", option);
+      usage();
+      exit(1);
+    }
+  }
 
   RAM* ram = new_ram();
   CPU* cpu = new_cpu();
