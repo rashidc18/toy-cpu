9eea4724074730a31023c82820f265ec182b3c43
Rashid <rashid18008@outlook.com>
2026-04-29 15:47:49 -0300

    update(install): add message functions and stop if error

--- install.sh
+++ install.sh
@@ -3,12 +3,35 @@
 TOYCPU=toycpu
 TOYCPU_STRING="Toy CPU"
 DSL=asmdsl.rb
+RUBY_PATH=/usr/lib64/ruby/
 
-echo "** Installing Ruby DSL."
-cp $DSL /usr/lib64/ruby/$DSL
+message() {
+  echo "**** $1. ****"
+}
 
-echo "** Building $TOYCPU_STRING."
+install_message() {
+  message "Installing $1"
+}
+
+building_message() {
+  message "Building $1"
+}
+
+success_message() {
+  message "Success"
+}
+
+install_message "Ruby DSL"
+cp $DSL $RUBY_PATH$DSL
+set -e
+success_message
+
+building_message $TOYCPU_STRING
 make
+set -e
+success_message
 
-echo "** Installing $TOYCPU_STRING."
+install_message $TOYCPU_STRING
 mv $TOYCPU /usr/local/bin/$TOYCPU
+set -e
+success_message
