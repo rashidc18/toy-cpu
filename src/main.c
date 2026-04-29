8102773faa163f4dea333a3d9d2c30bef01412b4
Rashid <rashid18008@outlook.com>
2026-04-29 12:33:48 -0300

    remove(main): debug information

--- src/main.c
+++ src/main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #include "cpu.h"
 #include "ram.h"
 #include "error.h"
 #include "loader.h"
 #include "run.h"
-
-#define DEBUG 0
 
 void usage() {
   printf("Usage: toycpu [options] File...\n");
@@ -27,19 +24,7 @@ int main(int argc, char* argv[]) {
 
   RAM* ram = new_ram();
   CPU* cpu = new_cpu();
-
-  struct timespec load_start_time, load_end_time;
-
-  clock_gettime(CLOCK_MONOTONIC, &load_start_time);
   load_ram_from_file(ram, file);
-  clock_gettime(CLOCK_MONOTONIC, &load_end_time);
-
-  double load_time = 
-    (load_end_time.tv_sec - load_start_time.tv_sec) + 
-    (load_end_time.tv_nsec - load_start_time.tv_nsec) / 1e9;
-
-  if (DEBUG)
-    printf("[%f] File %s loaded into memory successfully.\n", load_time, file);
 
   cpu->on = 1;
 
