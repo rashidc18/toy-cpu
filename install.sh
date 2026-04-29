d8d44ca5cd63504802ffd40530fabb735f183cbd
Rashid <rashid18008@outlook.com>
2026-04-29 15:33:33 -0300

    feat(install): add ruby lib

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
 TOYCPU=toycpu
+TOYCPU_STRING="Toy CPU"
+DSL=asmdsl.rb
 
+echo "** Installing Ruby DSL."
+cp $DSL /usr/lib64/ruby/$DSL
+
+echo "** Building $TOYCPU_STRING."
 make
-sudo mv $TOYCPU /usr/local/bin/$TOYCPU
+
+echo "** Installing $TOYCPU_STRING."
+mv $TOYCPU /usr/local/bin/$TOYCPU
